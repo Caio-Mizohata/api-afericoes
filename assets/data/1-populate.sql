0d6f5d6695d9f7206b963305fd0b1ee60b79f6ed
Caio Mizohata <134729130+Caio-Mizohata@users.noreply.github.com>
2025-11-03 04:51:37 +0000

    FEAT: Auth

--- assets/data/1-populate.sql
+++ assets/data/1-populate.sql
@@ -1,3 +1,3 @@
 USE application;
 
-INSERT INTO users(nome_de_usuario, senha) VALUES ('admin', 'root');
+INSERT INTO usuarios(nome_de_usuario, senha) VALUES ('admin', '$2a$12$cIZbepApcAPqqksaLoRjm.CQwEgtK6H6qmgalKBDP2mzzsW.Y.P5q');
