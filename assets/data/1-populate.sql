9c5f1925023b23f7e4e4587531188bfd15ecbb49
Caio Mizohata <134729130+Caio-Mizohata@users.noreply.github.com>
2025-11-04 17:01:07 +0000

    Feat: Hash Bycrypt do admin adicionado

--- assets/data/1-populate.sql
+++ assets/data/1-populate.sql
@@ -1,3 +1,4 @@
 USE application;
 
-INSERT INTO usuarios(nome_de_usuario, senha) VALUES ('admin', '$2a$12$cIZbepApcAPqqksaLoRjm.CQwEgtK6H6qmgalKBDP2mzzsW.Y.P5q');
+-- admin & bycrypt hash no 'admin'
+INSERT INTO usuarios(nome_de_usuario, senha) VALUES ('admin', '$2a$12$RDSpEQV9nFFsuOjymRXMU.Nni1p8WYYJA7FbDdHaHWacPbL/qB8AS');
